a8da90fee2bb4d93da8af67fa2f2333e97a6be17
naderdl <naderdaliri@gmail.com>
2018-02-19 13:53:04 +0330

    docs: test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require("gulp");
+const nodemon = require("gulp-nodemon");
 
 gulp.task("default", () => {
   console.log("nothing here");
+});
+gulp.task("start", () => {
+  nodemon({
+    script: "src/server.js",
+    ext: "js html",
+    env: { NODE_ENV: "development" }
+  });
 });
