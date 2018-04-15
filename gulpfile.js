ce27e02711ab41c73f51b632b7a8b8e0531611aa
naderdl <naderdaliri@gmail.com>
2018-04-15 14:34:52 +0430

    main change

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require("gulp");
 const nodemon = require("gulp-nodemon");
+const eslint = require("gulp-eslint");
 
 gulp.task("default", () => {
   console.log("nothing here");
 });
 gulp.task("start", () => {
   nodemon({
-    script: "src/server.js",
+    script: "build/server.js",
     ext: "js html",
     env: { NODE_ENV: "development" }
   });
 });
+gulp.task("lint", () =>
+  gulp
+    .src(["src/*.js", "!node_modules/**"])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
